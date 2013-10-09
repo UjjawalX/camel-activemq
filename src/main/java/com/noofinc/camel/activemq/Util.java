29676bef3498f39e62472ecbcba9f39eb44736da
jimternet <jamesbeyers@gmail.com>
2013-10-08 21:24:52 -0500

    First commit

---
+++ src/main/java/com/noofinc/camel/activemq/Util.java
@@ -0,0 +1,20 @@
+package com.noofinc.camel.activemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Util {
+
+	/**
+	 * @param args
+	 */
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+	
+	public void nothing(){
+		ActiveMQConnectionFactory fact;
+		org.apache.camel.component.jms.JmsConfiguration config;
+	}
+
+}
